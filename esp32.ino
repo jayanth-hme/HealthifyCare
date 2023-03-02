9194854d20b7b6e34d7927c9776b59eb072fc149
jayanth-hme <jayanth.av@healthifyme.com>
2023-03-02 14:53:02 +0530

    undo

--- esp32.ino
+++ esp32.ino
@@ -129,7 +129,6 @@ void connectWifi() {
     Serial.println(ssid);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
-      
         delay(500);
         Serial.print(".");
     }
