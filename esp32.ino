481d0e0a25ad1b62e405999b06aed38b0a4562e3
jayanth-hme <jayanth.av@healthifyme.com>
2023-03-02 14:29:26 +0530

    Tst

--- esp32.ino
+++ esp32.ino
@@ -129,12 +129,13 @@ void connectWifi() {
     Serial.println(ssid);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
+      
         delay(500);
         Serial.print(".");
     }
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
-    
+
     Serial.println(WiFi.localIP());
 }
